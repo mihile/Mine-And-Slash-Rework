b81c32ec316c2f2235655ceac53ab0cd4c54b786
mihile <93773182+mihile@users.noreply.github.com>
2026-05-21 12:09:23 +0900

    Port summon duration and expire handling

--- src/main/java/com/robertx22/mine_and_slash/database/data/spells/components/actions/SummonPetAction.java
+++ src/main/java/com/robertx22/mine_and_slash/database/data/spells/components/actions/SummonPetAction.java
@@ -29,6 +29,8 @@ public class SummonPetAction extends SpellAction {
         super(Arrays.asList());
     }
 
+    public static int INFINITE_DURATION = -1;
+
     @Override
     public void tryActivate(Collection<LivingEntity> targets, SpellCtx ctx, MapHolder data) {
 
@@ -54,8 +56,7 @@ public class SummonPetAction extends SpellAction {
 
             en.setPos(pos.x, pos.y, pos.z);
 
-            int duration = data.get(MapField.LIFESPAN_TICKS).intValue();
-            duration *= ctx.calculatedSpellData.data.getNumber(EventData.DURATION_MULTI, 1).number;
+            int duration = getDuration(ctx, data);
 
             float aggroRadius = ctx.calculatedSpellData.data.getNumber(EventData.AGGRO_RADIUS, 15).number;
             aggroRadius *= ctx.calculatedSpellData.data.getNumber(EventData.AGGRO_RADIUS_MULTI, 1).number;
@@ -77,6 +78,15 @@ public class SummonPetAction extends SpellAction {
 
         int totalSummons = (int) ctx.calculatedSpellData.data.getNumber(EventData.BONUS_TOTAL_SUMMONS, 0).number;
         updatePlayerSummons(ctx.caster, totalSummons, ctx.calculatedSpellData.spell_id);
+    }
+
+    private static int getDuration(SpellCtx ctx, MapHolder data) {
+        int duration = data.get(MapField.LIFESPAN_TICKS).intValue();
+        if (duration == INFINITE_DURATION) {
+            return duration;
+        }
+
+        return (int) (duration * ctx.calculatedSpellData.data.getNumber(EventData.DURATION_MULTI, 1).number);
     }
 
     public static void updatePlayerSummons(LivingEntity caster, int totalSummons, String currentSummonSpell) {
